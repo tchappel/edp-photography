5c8a97b0a93d3fd7ee2205144d7c149f85efb1c3
tchappel <thomas.chappel4@gmail.com>
2025-11-28 14:46:04 +0100

    fix footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,30 +1,41 @@
 import { Container } from "@/components/container";
 import { Link } from "@/components/link";
 import { routes } from "@/lib/routes";
+import { cn } from "@/lib/utils";
 import { SocialLinks } from "../social-links";
 
 export function Footer() {
   return (
     <Container
       component="footer"
-      className="flex w-full py-4 flex-col items-center gap-4 sm:grid sm:grid-cols-3"
+      className={cn(
+        "grid grid-cols-1 justify-items-center items-center gap-2 sm:grid-cols-3",
+        "py-4"
+      )}
     >
+      <SocialLinks className="sm:order-2" />
+      <div className={cn("sm:order-3 sm:justify-self-end")}>
+        <Link
+          href={routes.contact}
+          underline="none"
+          variant="h6"
+          className={cn("font-bold", "inline-block p-2")}
+        >
+          CONTACT
+        </Link>
+      </div>
+
       <Link
         href={routes.home}
         underline="none"
         variant="h6"
-        className="font-bold p-2 order-3 sm:order-1"
+        className={cn(
+          "font-bold",
+          "inline-block p-2",
+          "sm:order-1 sm:justify-self-start"
+        )}
       >
         © EMANUEL DELLA PIA
-      </Link>
-      <SocialLinks className="order-1 sm:order-2" />
-      <Link
-        href={routes.contact}
-        underline="none"
-        variant="h6"
-        className="font-bold p-2 order-3 sm:order-3 sm:justify-self-end"
-      >
-        CONTACT
       </Link>
     </Container>
   );
